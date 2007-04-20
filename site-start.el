21b39ec4a93e01e643f61a865b83bd29870ab3d8
lang <lang>
2007-04-20 16:04:07 +0000

    mlm: attempt to setup mmm-mode

--- site-start.el
+++ site-start.el
@@ -133,34 +133,26 @@ Set it intead of tab-width.")
   (rc-maybe-session))
 
 (defun rc-maybe-mmm-mode ()
-  "Setup several modes for asp javascript and vbscript"
   (interactive)
   (if (require 'mmm-mode "mmm-mode" t)
-      (setq mmm-global-mode 'maybe)
+      (setq mmm-global-mode 'maybe)))
 
-    (mmm-add-group
-     'fancy-html
-     '((html-php-tagged
-        :submode php-mode
-        :face mmm-code-submode-face
-        :front "<[?]php"
-        :back "[?]>")
-       (html-css-attribute
-        :submode css-mode
-        :face mmm-declaration-submode-face
-        :front "style=\""
-        :back "\"")))
-
-    ;; What files to invoke the new html-mode for?
-    (add-to-list 'auto-mode-alist '("\\.inc\\'" . html-mode))
-    (add-to-list 'auto-mode-alist '("\\.phtml\\'" . html-mode))
-    (add-to-list 'auto-mode-alist '("\\.php[34]?\\'" . html-mode))
-    (add-to-list 'auto-mode-alist '("\\.[sj]?html?\\'" . html-mode))
-    (add-to-list 'auto-mode-alist '("\\.jsp\\'" . html-mode))
-    ;; What features should be turned on in this html-mode?
-    (add-to-list 'mmm-mode-ext-classes-alist '(html-mode nil html-js))
-    (add-to-list 'mmm-mode-ext-classes-alist '(html-mode nil embedded-css))
-    (add-to-list 'mmm-mode-ext-classes-alist '(html-mode nil fancy-html))))
+(defun rc-asp-javascript-mode ()
+  "Broken still"
+  (interactive)
+  (rc-maybe-mmm-mode)
+  (mmm-add-group
+   'asp-javascript
+   '((asp-code
+      :submode javascript-mode
+      :face mmm-code-submode-face
+      :front "<%"
+      :back "%>")))
+  (add-to-list 'auto-mode-alist '("\\.as[ap]\\'" . asp-javascript-mode))
+  (add-to-list 'auto-mode-alist '("\\.inc\\'" . asp-javascript-mode))
+  (add-to-list 'mmm-mode-ext-classes-alist '(asp-javascript-mode nil html-js))
+  (add-to-list 'mmm-mode-ext-classes-alist '(asp-javascript-mode nil embedded-css))
+  (add-to-list 'mmm-mode-ext-classes-alist '(asp-javascript-mode nil asp-javascript)))
 
 (defun rc-utf8 ()
   "Setup terminal for UTF-8"
