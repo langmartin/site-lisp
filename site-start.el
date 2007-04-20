4c541e96d93a605767c36b0788a215fa61af65ab
lang <lang>
2007-04-20 15:29:10 +0000

    mlm: mmm mode added, we'll get there soon for asp modes

--- site-start.el
+++ site-start.el
@@ -132,6 +132,36 @@ Set it intead of tab-width.")
   (require 'javascript-mode "javascript-mode" t)
   (rc-maybe-session))
 
+(defun rc-maybe-mmm-mode ()
+  "Setup several modes for asp javascript and vbscript"
+  (interactive)
+  (if (require 'mmm-mode "mmm-mode" t)
+      (setq mmm-global-mode 'maybe)
+
+    (mmm-add-group
+     'fancy-html
+     '((html-php-tagged
+        :submode php-mode
+        :face mmm-code-submode-face
+        :front "<[?]php"
+        :back "[?]>")
+       (html-css-attribute
+        :submode css-mode
+        :face mmm-declaration-submode-face
+        :front "style=\""
+        :back "\"")))
+
+    ;; What files to invoke the new html-mode for?
+    (add-to-list 'auto-mode-alist '("\\.inc\\'" . html-mode))
+    (add-to-list 'auto-mode-alist '("\\.phtml\\'" . html-mode))
+    (add-to-list 'auto-mode-alist '("\\.php[34]?\\'" . html-mode))
+    (add-to-list 'auto-mode-alist '("\\.[sj]?html?\\'" . html-mode))
+    (add-to-list 'auto-mode-alist '("\\.jsp\\'" . html-mode))
+    ;; What features should be turned on in this html-mode?
+    (add-to-list 'mmm-mode-ext-classes-alist '(html-mode nil html-js))
+    (add-to-list 'mmm-mode-ext-classes-alist '(html-mode nil embedded-css))
+    (add-to-list 'mmm-mode-ext-classes-alist '(html-mode nil fancy-html))))
+
 (defun rc-utf8 ()
   "Setup terminal for UTF-8"
   (interactive)
