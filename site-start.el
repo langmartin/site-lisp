c0844b1b46bf64f7b03b37f2c521a0d86b42e6f1
lang <lang>
2007-08-03 18:31:02 +0000

    mlm: comment out darcs VC (not useful) and mmm-mode loading (not working)

--- site-start.el
+++ site-start.el
@@ -113,8 +113,8 @@ Set it intead of tab-width.")
 
   ;;; Load if it's there
   (require 'php-mode "php-mode" t)
-  (add-to-list 'vc-handled-backends 'DARCS)
-  (load "darcsum" t)
+  ;; (add-to-list 'vc-handled-backends 'DARCS)
+  ;; (load "darcsum" t)
   (if (require 'css-mode "css-mode-fixed" t)
       (progn
         (setq auto-mode-alist
@@ -243,33 +243,33 @@ Set it intead of tab-width.")
     "Redefined to empty function so that movement commands with cursor break the undo list"))
 
 ;;;; Unstable
-(defun rc-maybe-mmm-mode ()
-  (interactive)
-  (if (require 'mmm-auto "mmm-mode" t)
-      (progn
-        (setq mmm-global-mode 'maybe)
-        (mmm-add-classes
-         '((asp-javascript
-            :submode javascript-mode
-            :face mmm-code-submode-face
-            :front "<%"
-            :back "%>")
-           (asp-vbscript
-            :submode visual-basic-mode
-            :face mmm-code-submode-face
-            :front "<%"
-            :back "%>")))
-        (define-derived-mode asp-mode html-mode "ASP/HTML"))))
+;; (defun rc-maybe-mmm-mode ()
+;;   (interactive)
+;;   (if (require 'mmm-auto "mmm-mode" t)
+;;       (progn
+;;         (setq mmm-global-mode 'maybe)
+;;         (mmm-add-classes
+;;          '((asp-javascript
+;;             :submode javascript-mode
+;;             :face mmm-code-submode-face
+;;             :front "<%"
+;;             :back "%>")
+;;            (asp-vbscript
+;;             :submode visual-basic-mode
+;;             :face mmm-code-submode-face
+;;             :front "<%"
+;;             :back "%>")))
+;;         (define-derived-mode asp-mode html-mode "ASP/HTML"))))
 
-(defun rc-asp-javascript-mode ()
-  "Broken still"
-  (interactive)
-  (rc-maybe-mmm-mode)
-  (add-to-list 'auto-mode-alist '("\\.as[ap]\\'" . asp-mode))
-  (add-to-list 'auto-mode-alist '("\\.inc\\'" . asp-mode))
-  (add-to-list 'mmm-mode-ext-classes-alist '(asp-mode nil html-js))
-  (add-to-list 'mmm-mode-ext-classes-alist '(asp-mode nil embedded-css))
-  (add-to-list 'mmm-mode-ext-classes-alist '(asp-mode nil asp-javascript)))
+;; (defun rc-asp-javascript-mode ()
+;;   "Broken still"
+;;   (interactive)
+;;   (rc-maybe-mmm-mode)
+;;   (add-to-list 'auto-mode-alist '("\\.as[ap]\\'" . asp-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.inc\\'" . asp-mode))
+;;   (add-to-list 'mmm-mode-ext-classes-alist '(asp-mode nil html-js))
+;;   (add-to-list 'mmm-mode-ext-classes-alist '(asp-mode nil embedded-css))
+;;   (add-to-list 'mmm-mode-ext-classes-alist '(asp-mode nil asp-javascript)))
 
 ;;;; Shell customization
 (defun kill-invisible-shell-buffers (&optional vis)
