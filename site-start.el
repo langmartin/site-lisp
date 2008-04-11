532b592afcb8759e9bbe721e57439a868afec447
Lang Martin <lang@lang.coptix.lan>
2008-04-11 10:38:20 -0400

    move unstable out of site-start

--- site-start.el
+++ site-start.el
@@ -377,35 +377,6 @@ like GNU screen with a C-t command key."
   ;; (funcall set-key "\C-tb" 'mark-and-search-backward)
   )
 
-;;;; Unstable
-;; (defun rc-maybe-mmm-mode ()
-;;   (interactive)
-;;   (if (require 'mmm-auto "mmm-mode" t)
-;;       (progn
-;;         (setq mmm-global-mode 'maybe)
-;;         (mmm-add-classes
-;;          '((asp-javascript
-;;             :submode javascript-mode
-;;             :face mmm-code-submode-face
-;;             :front "<%"
-;;             :back "%>")
-;;            (asp-vbscript
-;;             :submode visual-basic-mode
-;;             :face mmm-code-submode-face
-;;             :front "<%"
-;;             :back "%>")))
-;;         (define-derived-mode asp-mode html-mode "ASP/HTML"))))
-
-;; (defun rc-asp-javascript-mode ()
-;;   "Broken still"
-;;   (interactive)
-;;   (rc-maybe-mmm-mode)
-;;   (add-to-list 'auto-mode-alist '("\\.as[ap]\\'" . asp-mode))
-;;   (add-to-list 'auto-mode-alist '("\\.inc\\'" . asp-mode))
-;;   (add-to-list 'mmm-mode-ext-classes-alist '(asp-mode nil html-js))
-;;   (add-to-list 'mmm-mode-ext-classes-alist '(asp-mode nil embedded-css))
-;;   (add-to-list 'mmm-mode-ext-classes-alist '(asp-mode nil asp-javascript)))
-
 ;;;; Shell customization
 (defun kill-invisible-shell-buffers (&optional vis)
   (interactive)
