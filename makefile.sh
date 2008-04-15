a0016bb968f09ec87dc2829a631193b852ced0e2
Lang Martin <lang@coptix.com>
2008-04-15 09:47:35 -0400

    remove options to get this to compile on jerk

--- makefile.sh
+++ makefile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-libs="-l tramp -l buffer-time-stamp"
+libs="-l buffer-time-stamp"
 
 cat source-file-list.txt | while read file; do
 	emacs -q --no-site-file $libs -batch -f batch-byte-compile "$file" || exit
