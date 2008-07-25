d96f8e7229b9d859ee430f6849a785fcf0d062ff
Lang Martin <lang@lang.coptix.lan>
2008-07-25 13:52:39 -0400

    fix makefile to be more make-like, not rebuild files unless necessary

--- makefile.sh
+++ makefile.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 emacs="$1"; shift
 [ -z "$emacs" ] && emacs=`which emacs`
-libs="-l buffer-time-stamp.el"
+libs="-l buffer-time-stamp.el -L ."
 
 cat source-file-list.txt | while read file; do
+	[ "$file" -nt "${file}c" ] || continue
+	echo "$file: "
 	"$emacs" -q --no-site-file \
 		$libs \
 		-batch -f batch-byte-compile "$file" || exit
