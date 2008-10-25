b548d748fb4ff75c73e85a3b00548103096dc3bf
Lang Martin <lang@lang.local>
2008-10-25 12:50:34 -0400

    move mail configs I want to keep out to a new file

--- lang-dot-emacs.el
+++ lang-dot-emacs.el
@@ -80,23 +80,6 @@
 
 (global-set-key "\C-z" nil)
 
-(defun rc-gnus ()
-  (require 'gnus)
-  (setq gnus-invalid-group-regexp "[:`'\"]\\|^$"
-        gnus-ignored-newsgroups "")
-  (require 'nnimap)
-  (setq gnus-select-method
-        '(nnimap "coptix"
-                 (nnimap-address "imap.gmail.com")
-                 (nnimap-server-port 993)
-                 (nnimap-stream ssl)))
-  (setq gnus-secondary-select-methods
-        '((nntp "newsgroups.bellsouth.net")))
-  (setq gnus-thread-sort-functions '((not gnus-thread-sort-by-number)))
-  (add-hooks '(gnus-summary-mode-hook)
-             (lambda ()
-               (rc-screen-ify-control-t 'local-set-key))))
-
 (defun mail-send-and-exit-kill (&optional arg)
   "send mail message, bury the buffer and kill it. See mail-send-and-exit."
   (interactive "P")
@@ -124,60 +107,14 @@
 
 (rc-gmail-smtp)
 
-(defun pykk-init ()
-  (interactive)
-  (insert-file-contents "~/proj/pykk-interactive.py")
-  (end-of-buffer)
-  (comint-send-input))
+;; (defun pykk-init ()
+;;   (interactive)
+;;   (insert-file-contents "~/proj/pykk-interactive.py")
+;;   (end-of-buffer)
+;;   (comint-send-input))
 
-(add-hook 'inferior-python-mode-hook
-          (lambda () (local-set-key "\C-cp" 'pykk-init)))
-
-(defun rc-wanderlust ()
-  (autoload 'wl "wl" "Wanderlust" t)
-  (autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
-  (autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
-
-  ;; IMAP
-  (setq elmo-imap4-default-server "imap.gmail.com")
-  (setq elmo-imap4-default-user "lang.martin@coptix.com") 
-  (setq elmo-imap4-default-authenticate-type 'clear) 
-  (setq elmo-imap4-default-port '993)
-  (setq elmo-imap4-default-stream-type 'ssl)
-  (setq elmo-imap4-use-modified-utf7 t) 
-
-  ;; SMTP
-  (setq wl-smtp-connection-type 'starttls)
-  (setq wl-smtp-posting-port 587)
-  (setq wl-smtp-authenticate-type "plain")
-  (setq wl-smtp-posting-user "lang.martin@coptix.com")
-  (setq wl-smtp-posting-server "smtp.gmail.com")
-  (setq wl-local-domain "coptix.com")
-
-  (setq wl-default-folder "%inbox")
-  (setq wl-default-spec "%")
-  (setq wl-draft-folder ".drafts")
-  (setq wl-trash-folder ".trash")
-
-  (setq wl-folder-check-async t) 
-
-  (setq elmo-imap4-use-modified-utf7 t)
-
-  (autoload 'wl-user-agent-compose "wl-draft" nil t)
-  (if (boundp 'mail-user-agent)
-      (setq mail-user-agent 'wl-user-agent))
-  (if (fboundp 'define-mail-user-agent)
-      (define-mail-user-agent
-        'wl-user-agent
-        'wl-user-agent-compose
-        'wl-draft-send
-        'wl-draft-kill
-        'mail-send-hook))
-
-  (setq wl-icon-directory
-        "/Applications/Emacs.app/Contents/Resources/etc/wl/icons")
-  (setq wl-highlight-folder-with-icon nil)
-  )
+;; (add-hook 'inferior-python-mode-hook
+;;           (lambda () (local-set-key "\C-cp" 'pykk-init)))
 
 (defun end-of-linep ()
   (looking-at "$"))
