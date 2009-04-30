209c0ee57f9051635e3e0dff157d125dbd689461
Lang Martin <lmartin@tva.gov>
2009-04-30 16:39:54 -0400

    minor changes

--- themes/color-theme-langmartin.el
+++ themes/color-theme-langmartin.el
@@ -2,18 +2,21 @@
   "Color theme by Lang Martin, derived from color-theme-bharadwaj"
   (interactive)
   (color-theme-bharadwaj)
-  (let ((color-theme-is-cumulative t))
+  (let ((color-theme-is-cumulative t)
+        (match '(:background "lightgoldenrod2"))
+        (string '(:foreground "DarkRed")))
    (color-theme-install
-    '(color-theme-langmartin
+    `(color-theme-langmartin
       ((background-color . "ivory"))
       nil
       (erc-timestamp-face ((t (:bold t :foreground "green4"))))
-      (erc-current-nick-face ((t (:bold t :foreground "Turquoise3"))))
-      (font-lock-comment-face ((t (:foreground "grey45"))))
+      (erc-current-nick-face ((t (:bold t :foreground "Turquoise4"))))
+      (eshell-prompt-face ((t (:bold t :foreground "DarkRed"))))
+      (font-lock-comment-face ((t (:foreground "grey50"))))
       (font-lock-function-name-face ((t (:bold t :foreground "SlateBlue"))))
-      (match ((t (:background "lightgoldenrod2"))))
-      (minibuffer-noticeable-prompt ((t (:background "lightgoldenrod2"))))
-      (org-agenda-restriction-lock ((t (:background "lightgoldenrod2"))))
-      (org-clock-overlay ((t (:background "lightgoldenrod2"))))
-      (org-todo ((t (:bold t :foreground "Red3"))))
+      (match ((t ,match)))
+      (minibuffer-noticeable-prompt ((t ,match)))
+      (org-agenda-restriction-lock ((t ,match)))
+      (org-clock-overlay ((t ,match)))
+      (org-todo ((t ,string)))
       ))))
