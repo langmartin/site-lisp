5035595c8b447a54817fa1523019ce44c9ed29ab
Lang Martin <lmartin@tva.gov>
2009-05-01 14:43:24 -0400

    move the color theme into my dot-emacs for now

--- lang-dot-emacs.el
+++ lang-dot-emacs.el
@@ -29,6 +29,33 @@
   ;; (color-theme-montz)
   ;; (color-theme-shaman)
   ;; (color-theme-bharadwaj)
+
+  (defun color-theme-langmartin ()
+    (interactive)
+    (color-theme-bharadwaj)
+    (let ((color-theme-is-cumulative t)
+          (match '((t (:background "lightgoldenrod2"))))
+          (string '((t (:foreground "DarkRed"))))
+          (string-b '((t (:bold t :foreground "DarkRed"))))
+          (comment '((t (:foreground "grey50"))))
+          )
+      (color-theme-install
+       `(color-theme-langmartin
+         ((background-color . "ivory"))
+         nil
+         (erc-current-nick-face ((t (:bold t :foreground "Turquoise4"))))
+         (erc-notice-face ,comment)
+         (erc-timestamp-face ((t (:bold t :foreground "green4"))))
+         (eshell-prompt ,string-b)
+         (font-lock-comment-face ,comment)
+         (font-lock-function-name-face ((t (:bold t :foreground "SlateBlue"))))
+         (match ,match)
+         (minibuffer-noticeable-prompt ,match)
+         (org-agenda-restriction-lock ,match)
+         (org-clock-overlay ,match)
+         (org-todo ,string)
+         ))))
+  
   (color-theme-langmartin)
   )
 
