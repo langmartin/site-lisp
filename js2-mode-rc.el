a89bfc7b922f01eb231ba75bb6090953c54c4d5b
Lang Martin <lmartin@tva.gov>
2009-05-11 15:53:19 -0400

    js2-mode-rc.el: use customize after all

--- js2-mode-rc.el
+++ js2-mode-rc.el
@@ -1,6 +1,10 @@
 (require 'js2-mode)
 
-(set-variables
+;;; if you just set these, they're buffer local. If you stick them in
+;;; the hook, they're not overridable (easily). So, we do a second
+;;; customize block.
+
+(custom-set-variables
  '(js2-basic-offset 2)
  '(js2-bounce-indent-flag nil)
  )
