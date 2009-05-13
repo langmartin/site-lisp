85686221bb5feeb0c6444e38416f897952e9745a
Lang Martin <lmartin@tva.gov>
2009-05-13 12:31:19 -0400

    collect emacs 23 differences for easy reference
    Conflicts:
    
    	lang-dot-emacs.el

--- lang-dot-emacs.el
+++ lang-dot-emacs.el
@@ -1,42 +1,48 @@
+;;; things for cocoa emacs 23
+(progn
+  (require 'mwheel)
+  (global-set-key [wheel-down] 'mwheel-scroll)
+  (global-set-key [wheel-up] 'mwheel-scroll)
+  (global-set-key "\M-`" 'other-frame)
+  (require 'tramp))
+
 (load "site-start")
 
-;;; (require 'w3m-load)
+(progn
+ (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT"))
 
-(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT"))
+ (defun irc () "using erc"
+   (interactive)
+   (erc-select :server "irc.freenode.net"
+               :port 6667
+               :nick "langmartin"
 
-(defun irc () "using erc"
-  (interactive)
-  (erc-select :server "irc.freenode.net"
-              :port 6667
-              :nick "langmartin"
+               :full-name "Lang Martin"))
 
-              :full-name "Lang Martin"))
+ (add-hook 'erc-text-matched-hook 'erc-growl-match)
+ (erc-match-mode 1)
 
-(add-hook 'erc-text-matched-hook 'erc-growl-match)
-(erc-match-mode 1)
-
-(defun erc-hide-notices () "hide all notices in a very busy channel"
-  (interactive)
-  (make-local-variable 'erc-echo-notice-always-hook)
-  (setq erc-echo-notice-always-hook nil))
+ (defun erc-hide-notices () "hide all notices in a very busy channel"
+   (interactive)
+   (make-local-variable 'erc-echo-notice-always-hook)
+   (setq erc-echo-notice-always-hook nil)))
 
 (require 'kmacro)
 
-(defun color-grey () (color-theme-fischmeister) t)
-(defun color-slate () (color-theme-pok-wog) t)
-(and (require 'color-theme "color-theme" t)
-     (or (color-theme-initialize)
-         (color-grey)
-         (color-slate)))
+(progn
+ (defun color-grey () (color-theme-fischmeister) t)
+ (defun color-slate () (color-theme-subtle-hacker) t)
+ (and (require 'color-theme "color-theme" t)
+      (progn (color-theme-initialize)
+             ;; (color-slate)
+             ;; (show-paren-mode nil)
+             (color-grey)
+             )))
 
 (rc-lang)
 (global-set-key "\C-xl" (lambda () (interactive) (insert "lambda")))
 (global-set-key "\C-w" 'kill-backward-word-or-region)
 (global-set-key "\C-h" 'help)
-
-(require 'mwheel)
-(global-set-key [wheel-down] 'mwheel-scroll)
-(global-set-key [wheel-up] 'mwheel-scroll)
 
 (iswitchb-mode 1)
 
@@ -89,29 +95,12 @@
 ;; (add-hook 'inferior-python-mode-hook
 ;;           (lambda () (local-set-key "\C-cp" 'pykk-init)))
 
-(defun end-of-linep ()
-  (looking-at "$"))
-
-(defun underline-this-line (fill-character)
-  (interactive "cfill character:")
-  (let ((count 0))
-    (save-excursion
-      (beginning-of-line)
-      (previous-line)
-      (while (not (end-of-linep))
-        (setq count (+ count 1))
-        (forward-char)))
-    (dotimes (ii count)
-      (insert fill-character))))
-
-(global-set-key [kp-4] 'underline-this-line)
+(defun end-of-linep () (looking-at "$"))
 
 ;; (require 'toggle-letter-case)
 
 (require 'less)
-(add-hook 'find-file-hooks 'auto-less-minor-mode)
-
-(global-set-key "\M-`" 'other-frame)
+;; (add-hook 'find-file-hooks 'auto-less-minor-mode)
 
 (progn
   (require 'winner)
@@ -119,12 +108,12 @@
 
 (global-set-key "\C-x\C-b" 'ibuffer)
 
-(progn
-  (require 'bm)
-  (global-set-key [kp-3] 'bm-toggle)
-  (global-set-key [kp-6] 'bm-next)
-  (global-set-key [kp-9] 'bm-previous)
-  (global-set-key [kp-5] 'bm-show-all))
+;;; (progn
+;;;   (require 'bm)
+;;;   (global-set-key [kp-3] 'bm-toggle)
+;;;   (global-set-key [kp-6] 'bm-next)
+;;;   (global-set-key [kp-9] 'bm-previous)
+;;;   (global-set-key [kp-5] 'bm-show-all))
 
 (require 'rc-term-mode)
 (require 'rc-anything)
