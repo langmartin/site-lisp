fcf4ba199b07f68f63fbe3b9fc4c1a8e53f706e3
Lang Martin <lmartin@tva.gov>
2009-06-26 17:36:55 -0400

    lang-dot-emacs: keys for chop.el

--- lang-dot-emacs.el
+++ lang-dot-emacs.el
@@ -152,6 +152,12 @@
    ("<f12>" . rc-sgml-mode-for-asp)))
 
 (progn
+  (require 'chop)
+  (global-set-keys
+   '(("<C-up>" . chop-move-up)
+     ("<C-down>" . chop-move-down))))
+
+(progn
   (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
   (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
   (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
