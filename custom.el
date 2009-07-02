38ebaa418824acfb6266e0990312995a2eaf4f58
Lang Martin <lmartin@tva.gov>
2009-07-02 11:10:52 -0400

    new file: custom.el

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(dired-listing-switches "-alh")
+ '(erc-autojoin-channels-alist (quote (("freenode.net" "#medium" "#emacs" "#scheme"))))
+ '(erc-join-buffer (quote bury))
+ '(erc-server-reconnect-timeout 300)
+ '(js2-basic-offset 2)
+ '(js2-bounce-indent-flag nil)
+ '(org-agenda-files (quote ("z:/IDMWS_Int/dds/docs/spec.txt")))
+ '(show-trailing-whitespace t))
+
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
