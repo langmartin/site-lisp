dac12ae91804bbb4928746ef445c173eed4e9dcb
Lang Martin <lmartin@tva.gov>
2009-07-31 10:23:15 -0400

    erc-notice-face doesn't work any better as custom, it's frame dependant, I should figure it out eventually.

--- custom.el
+++ custom.el
@@ -56,4 +56,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(erc-notice-face ((t (:foreground "grey70")))))
+)
