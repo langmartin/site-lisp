0786c793dcf6ccef30a31992263a7643f11f6115
Lang Martin <lmartin@tva.gov>
2009-11-18 09:46:12 -0500

    new commands to copy and kill single lines adviced around C-w and M-w

--- lang-dot-emacs.el
+++ lang-dot-emacs.el
@@ -213,3 +213,19 @@
 (progn
   (require 'goto-last-change)
   (global-set-key "\C-x\C-\\" 'goto-last-change))
+
+;;;; http://emacs-fu.blogspot.com/2009/11/copying-lines-without-selecting-them.html
+(defadvice kill-ring-save (before slick-copy activate compile) "When called
+  interactively with no active region, copy a single line instead."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (message "Copied line")
+     (list (line-beginning-position) (line-beginning-position 2)))))
+
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single
+line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position) (line-beginning-position 2)))))
