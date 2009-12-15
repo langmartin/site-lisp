bfd5708b9b57108d94825ecd208177317acddd99
Lang Martin <lmartin@tva.gov>
2009-12-15 15:12:53 -0500

    deleted:    moz-rc.el modified:   rc-javascript.el merge moz-rc into rc-javascript

--- rc-javascript.el
+++ rc-javascript.el
@@ -18,6 +18,37 @@
 
 (add-hook 'js2-mode-hook 'turn-off-indent-tabs-mode)
 (add-hook 'js2-mode-hook 'turn-on-c-subword-mode)
-(provide 'js2-mode-rc)
 
+(require 'moz)
 
+(defun turn-on-moz-minor-mode ()
+  (moz-minor-mode 1))
+
+(add-hook 'js2-mode-hook 'turn-on-moz-minor-mode)
+
+(defun moz-send-string (string)
+  "Send a string to Firefox via MozRepl."
+  (comint-send-string (inferior-moz-process)
+                      (concat moz-repl-name ".pushenv('printPrompt', 'inputMode'); "
+                              moz-repl-name ".setenv('printPrompt', false); "
+                              moz-repl-name ".setenv('inputMode', 'multiline'); "
+                              "undefined; \n"))
+  ;; Give the previous line a chance to be evaluated on its own.  If
+  ;; it gets concatenated to the following ones, we are doomed.
+  (sleep-for 0 1)
+  (comint-send-string (inferior-moz-process)
+                      string)
+  (comint-send-string (inferior-moz-process)
+                      "\n--end-remote-input\n")
+  (comint-send-string (inferior-moz-process)
+                      (concat moz-repl-name ".popenv('inputMode', 'printPrompt'); "
+                              "undefined; \n"))
+  (comint-send-string (inferior-moz-process)
+                      "\n--end-remote-input\n")
+  (display-buffer (process-buffer (inferior-moz-process))))
+
+(defun moz-reload-page ()
+  (interactive)
+  (moz-send-string "content.location.href = content.location.href\n"))
+
+(provide 'rc-javascript)
