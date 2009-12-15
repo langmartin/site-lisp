2688574eb23d4c0990381a19b83026d500645a76
Lang Martin <lmartin@tva.gov>
2009-12-15 15:15:04 -0500

    turn off smileys in gnus

--- gnus-rc.el
+++ gnus-rc.el
@@ -59,6 +59,7 @@
     (setq send-mail-function 'smtpmail-send-it
           message-send-mail-function 'smtpmail-send-it)
     ;; (setq smtpmail-debug-info nil smtpmail-debug-verb nil)
+    (setq gnus-treat-display-smileys nil)
     (add-hook 'mail-mode-hook 'visual-line-not-auto-fill)
     (add-hook 'message-mode-hook 'visual-line-not-auto-fill)
     (add-hook 'message-setup-hook
