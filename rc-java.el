da0accb07d431e2f4f249125333f7a020fd470fe
Lang Martin <lmartin@tva.gov>
2010-02-11 12:24:28 -0500

    new file rc-java.el: configure JDEE, with some notes on downloading all the required packages

---
+++ rc-java.el
@@ -0,0 +1,33 @@
+;; Java IDE support (JDEE)
+
+(defun set-c-basic-offset-2 () (setq c-basic-offset 2))
+(defun set-tab-width-2 () (setq tab-width 2))
+
+(add-hook 'java-mode-hook 'set-c-basic-offset-2)
+(add-hook 'java-mode-hook 'set-tab-width-2)
+
+(progn
+  (progn
+    ;; http://sourceforge.net/projects/cedet/files/
+    (add-to-list 'load-path (contrib "cedet-1.0pre6/common/"))
+    (add-to-list 'Info-directory-list (contrib "cedet-1.0pre6/"))
+    (load "cedet")
+    (global-ede-mode 1)
+    (semantic-load-enable-code-helpers)
+    (global-srecode-minor-mode 1))
+
+  (progn
+    ;; http://sourceforge.net/projects/ecb/files/
+    (add-to-list 'load-path (contrib "ecb-2.40/"))
+    (add-to-list 'Info-directory-list (contrib "ecb-2.40/"))
+    (require 'ecb))
+
+  (progn
+    ;; http://sourceforge.net/projects/jdee/files/
+    (add-to-list 'load-path (contrib "jdee-2.4.0.1/lisp/"))
+    (require 'jde)
+    (add-to-auto-mode-alist '(("\\.java\\'" . jde-mode)))
+    (add-hook 'jde-mode-hook 'c-basic-offset-2)))
+
+(provide 'rc-java)
+
