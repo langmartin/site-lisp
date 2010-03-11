4f1106a960507a4e6e65a3f25e59c3d76d1da321
Lang Martin <lmartin@tva.gov>
2010-03-11 15:28:47 -0500

    modified:   rc-jabber.el

--- rc-jabber.el
+++ rc-jabber.el
@@ -4,7 +4,7 @@
   (require 'jabber-autoloads)
   (add-to-list 'Info-directory-list (rc-contrib "emacs-jabber-0.8.0/")))
 
-(add-hook 'jabber-post-connect-hooks 'jabber-autoaway-start)
+;; (add-hook 'jabber-post-connect-hooks 'jabber-autoaway-start)
 
 (set-variables
  '(jabber-alert-presence-hooks nil)
@@ -14,7 +14,5 @@
  '(jabber-chat-prompt-foreign ((t (:foreground "red4"))))
  '(jabber-chat-prompt-local ((t (:foreground "blue4"))))
  '(jabber-chat-prompt-system ((t (:foreground "green4" :weight bold)))))
-
-(add-hook 'jabber-post-connect-hook 'jabber-autoaway-start)
 
 (provide 'rc-jabber)
