b8f762669d72e36c50a2bb760b889302958d8b79
Lang Martin <lang.martin@gmail.com>
2010-10-12 10:13:05 -0400

    csharp utilites added, which requires multi-mode and powershell. csharp-completion.el fixed to include semantic as it is distributed in emacs 23.

--- subdirs.el
+++ subdirs.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t -*-
 (normal-top-level-add-to-load-path
- '("scheme" "bbdb-vcard" "jd-el" "org-mode/lisp"))
+ '("scheme" "bbdb-vcard" "jd-el" "org-mode/lisp" "csharp"))
 (add-to-info-path "org-mode/doc")
 ;; Local Variables:
 ;; version-control: never
