6b0b9f848df52249cb65c732c192125ccbc9d56a
Lang Martin <lang.martin@gmail.com>
2011-01-12 14:22:58 -0500

    new submodule: anything-config

--- subdirs.el
+++ subdirs.el
@@ -1,14 +1,9 @@
 ;; -*- no-byte-compile: t -*-
 
-(normal-top-level-add-to-load-path
- '("scheme" "bbdb-vcard" "jd-el" "csharp" "vimvars"))
+(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+    (normal-top-level-add-subdirs-to-load-path))
 
-(normal-top-level-add-to-load-path '("org-mode/lisp"))
 (add-to-info-path "org-mode/doc")
-
-(normal-top-level-add-to-load-path '("slime" "slime/contrib" "swank-js"))
-
-(normal-top-level-add-to-load-path '("gnus/lisp"))
 (add-to-info-path "gnus/texi")
 
 ;; Local Variables:
