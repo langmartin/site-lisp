d793671fdd060f62dee835b3f5d2a73eb3e053fd
Lang Martin <lang.martin@gmail.com>
2011-01-20 15:33:46 -0500

    Revert "use process-file because it works with tramp"
    This reverts commit e9a8f2b043722e681c9f37df8a7584cd74955cde.

--- make-tags-file.el
+++ make-tags-file.el
@@ -82,7 +82,7 @@
 
 (defvar exec-etags "etags")
 (defun exec-etags (lst)
-  (apply 'process-file
+  (apply 'call-process
          exec-etags
          nil
          nil
@@ -94,7 +94,7 @@
   (let* ((name "*ctags*")
          (buffer (or (get-buffer name)
                      (generate-new-buffer name))))
-    (apply 'process-file
+    (apply 'call-process
            exec-exuberant-ctags
            nil
            `(,buffer t)
