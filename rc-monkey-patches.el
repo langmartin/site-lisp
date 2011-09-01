cacb0ec16dd9e44c54dfb5d8870f92854c03b1bc
Lang Martin <lang.martin@gmail.com>
2011-09-01 14:38:01 -0400

    rc-monkey-patches needs to requires stuff before attempting to overwrite the stuff's functions, duh

--- rc-monkey-patches.el
+++ rc-monkey-patches.el
@@ -1,24 +1,23 @@
-(if (boundp 'magit-interactive-rebase)
-    (progn 
-      (defvar magit-editor "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient")
-
-      (defun magit-interactive-rebase ()
-        "Start a git rebase -i session, old school-style."
-        (interactive)
-        (unless (magit-server-running-p)
-          (server-start))
-        (let* ((section (get-text-property (point) 'magit-section))
-               (commit (and (member 'commit (magit-section-context-type section))
-                            (magit-section-info section)))
-               (old-editor (getenv "GIT_EDITOR")))
-          (setenv "GIT_EDITOR" (or magit-editor
-                                   (locate-file "emacsclient" exec-path)))
-          (unwind-protect
-              (magit-run-git-async
-               "rebase" "-i"
-               (or (and commit (concat commit "^"))
-                   (magit-read-rev "Interactively rebase to" (magit-guess-branch))))
-            (if old-editor
-                (setenv "GIT_EDITOR" old-editor)))))))
+(progn 
+  (require 'magit)
+  (defvar magit-editor "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient")
+  (defun magit-interactive-rebase ()
+    "Start a git rebase -i session, old school-style."
+    (interactive)
+    (unless (magit-server-running-p)
+      (server-start))
+    (let* ((section (get-text-property (point) 'magit-section))
+           (commit (and (member 'commit (magit-section-context-type section))
+                        (magit-section-info section)))
+           (old-editor (getenv "GIT_EDITOR")))
+      (setenv "GIT_EDITOR" (or magit-editor
+                               (locate-file "emacsclient" exec-path)))
+      (unwind-protect
+          (magit-run-git-async
+           "rebase" "-i"
+           (or (and commit (concat commit "^"))
+               (magit-read-rev "Interactively rebase to" (magit-guess-branch))))
+        (if old-editor
+            (setenv "GIT_EDITOR" old-editor))))))
 
 (provide 'rc-monkey-patches)
