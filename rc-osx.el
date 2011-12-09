060b4e63243e2d1b5cef41770c1f21db1d514d46
Lang Martin <lang.martin@gmail.com>
2011-12-09 13:32:56 -0500

    don't point postgres at /command

--- rc-osx.el
+++ rc-osx.el
@@ -23,7 +23,6 @@
  '(ns-alternate-modifier (quote hyper))
  '(ns-command-modifier (quote meta))
  '(tramp-initial-commands (quote ("unset HISTFILE" "unset correct" "unset autocorrect")))
- '(sql-postgres-program "/command/psql")
  '(Info-additional-directory-list (quote ("/Applications/Emacs.app/Contents/Resources/info" "/usr/share/info")))
  '(woman-manpath (quote ("/usr/man" "/usr/share/man" "/usr/local/man" "/usr/local/share/man")))
  '(woman-use-own-frame nil))
