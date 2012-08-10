2598d727fa9abb022a4872376bb79b54c4823827
Lang Martin <lang.martin@gmail.com>
2012-08-10 15:11:54 -0400

    switch to ben's solution of embedding js-mode indentation in js2-mode

---
+++ embed-js-mode-js2.el
@@ -0,0 +1,74 @@
+;;;; Ben's vanilla js2-mode + js-mode's indent engine
+;; Adapted from:
+;; http://mihai.bazon.net/projects/editing-javascript-with-emacs-js2-mode/
+
+(defun custom-js2-indent-function ()
+  (interactive)
+  (save-restriction
+    (widen)
+    (let* ((inhibit-point-motion-hooks t)
+           (parse-status (save-excursion (syntax-ppss (point-at-bol))))
+           (offset (- (current-column) (current-indentation)))
+           (indentation (js--proper-indentation parse-status))
+           node)
+
+      (save-excursion
+        (back-to-indentation)
+
+        ;; consecutive declarations in a var statement are nice if
+        ;; properly aligned, i.e:
+        ;;
+        ;; var foo = "bar",
+        ;;     bar = "foo";
+        (setq node (js2-node-at-point))
+        (when (and node
+                   (= js2-NAME (js2-node-type node))
+                   (= js2-VAR (js2-node-type (js2-node-parent node))))
+          (setq indentation (+ 4 indentation))))
+
+      (indent-line-to indentation)
+      (when (> offset 0)
+        (forward-char offset)))))
+
+(defun generic-indent-sexp ()
+  (interactive)
+  (save-restriction
+    (save-excursion
+      (widen)
+      (let* ((inhibit-point-motion-hooks t)
+             (parse-status (syntax-ppss (point)))
+             (beg (nth 1 parse-status))
+             (end-marker (make-marker))
+             (end (progn (goto-char beg) (forward-list) (point)))
+             (ovl (make-overlay beg end)))
+        (set-marker end-marker end)
+        (overlay-put ovl 'face 'highlight)
+        (goto-char beg)
+        (while (< (point) (marker-position end-marker))
+          ;; don't reindent blank lines so we don't set the "buffer
+          ;; modified" property for nothing
+          (beginning-of-line)
+          (unless (looking-at "\\s-*$")
+            (indent-according-to-mode))
+          (forward-line))
+        (run-with-timer 0.5 nil '(lambda(ovl)
+                                   (delete-overlay ovl)) ovl)))))
+
+(defun embed-js-mode-into-js2 ()
+  (require 'js)
+  (require 'js2-highlight-vars)
+
+  (c-toggle-auto-state 0)
+  (c-toggle-hungry-state 1)
+
+  (set (make-local-variable 'indent-line-function) 'custom-js2-indent-function)
+
+  (define-key js2-mode-map [(return)] 'newline-and-indent)
+  (define-key js2-mode-map [(backspace)] 'c-electric-backspace)
+  (define-key js2-mode-map [(control d)] 'c-electric-delete-forward)
+  (define-key js2-mode-map [(meta q)] 'generic-indent-sexp)
+
+  (if (featurep 'js2-highlight-vars)
+      (js2-highlight-vars-mode)))
+
+(provide 'embed-js-mode-js2)
--- rc-javascript.el
+++ rc-javascript.el
@@ -18,10 +18,11 @@
    '(js2-global-externs (quote (require exports))))
   (add-hook 'js2-mode-hook 'turn-on-c-subword-mode)
   (define-key js2-mode-map (kbd "H-l") 'js-insert-lambda)
-  (eval-after-load 'js2-mode
-    '(progn
-       (require 'js2-imenu-extras)
-       (js2-imenu-extras-setup))))
+  ;; (eval-after-load 'js2-mode
+  ;;   '(progn
+  ;;      (require 'js2-imenu-extras)
+  ;;      (js2-imenu-extras-setup)))
+  )
 
 (defun rc-js2-mode-spaces ()
   "Configure js2 mode without tabs and with js2-basic-offset: 2"
@@ -42,6 +43,7 @@
   (rc-js2-mode-setup)
   (rc-js2-mode-tabs)
   (add-to-auto-mode-alist '(("\\.js\\'" . js2-mode)))
+  (add-hook 'js2-mode-hook 'embed-js-mode-into-js2)
   (defalias 'rc-javascript-mode 'js2-mode))
 
 (defun rc-espresso-mode ()
@@ -69,43 +71,8 @@
   (define-key js-mode-map (kbd "H-l") 'js-insert-lambda)
   (defalias 'rc-javascript-mode 'js-mode))
 
+(require 'embed-js-mode-js2)
+
 (rc-js2-mode)
-
-(defun rc-mozilla-repl ()
-  (require 'moz)
-  (defun turn-on-moz () (interactive) (moz-minor-mode 1))
-
-  (defun moz-send-string (string)
-    "Send a string to Firefox via MozRepl."
-    (comint-send-string (inferior-moz-process)
-                        (concat moz-repl-name ".pushenv('printPrompt', 'inputMode'); "
-                                moz-repl-name ".setenv('printPrompt', false); "
-                                moz-repl-name ".setenv('inputMode', 'multiline'); "
-                                "undefined; \n"))
-    ;; Give the previous line a chance to be evaluated on its own.  If
-    ;; it gets concatenated to the following ones, we are doomed.
-    (sleep-for 0 1)
-    (comint-send-string (inferior-moz-process)
-                        string)
-    (comint-send-string (inferior-moz-process)
-                        "\n--end-remote-input\n")
-    (comint-send-string (inferior-moz-process)
-                        (concat moz-repl-name ".popenv('inputMode', 'printPrompt'); "
-                                "undefined; \n"))
-    (comint-send-string (inferior-moz-process)
-                        "\n--end-remote-input\n")
-    (display-buffer (process-buffer (inferior-moz-process))))
-
-  (defun moz-reload ()
-    (interactive)
-    (moz-send-string "content.location.reload()\n"))
-
-  (defalias 'mozrepl-reload 'moz-reload)
-
-  (defun moz-enter ()
-    (interactive)
-    (moz-send-string "repl.enter(content)\n"))
-
-  (defalias 'mozrepl-enter 'moz-enter))
 
 (provide 'rc-javascript)
