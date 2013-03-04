a31a14dd4ce3f71b9b4899fb23b9c87d70e2419e
Lang Martin <lang.martin@gmail.com>
2013-03-04 09:15:33 -0500

    add melp package repository

--- rc-package.el
+++ rc-package.el
@@ -1,6 +1,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
 (defmacro with-feature (feature &rest body)
