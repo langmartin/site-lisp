cd7162051f6e891054131c068ced293f1c998ba0
Lang Martin <lang.martin@gmail.com>
2013-10-04 14:46:46 -0400

    just use marmalade for packages

--- rc-package.el
+++ rc-package.el
@@ -1,8 +1,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
+;; (add-to-list 'package-archives
+;;              '("melpa" . "http://melpa.milkbox.net/packages/"))
 
 (package-initialize)
 
