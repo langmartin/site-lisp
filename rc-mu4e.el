48334687f83f4fd99691c5c402787cbc8bed4298
Lang Martin <lang.martin@gmail.com>
2014-01-31 10:46:41 -0500

    rc-mu4e clean up customize a little bit

--- rc-mu4e.el
+++ rc-mu4e.el
@@ -36,22 +36,33 @@
 
 (define-key mu4e-view-mode-map "," 'mu4e-view-with-html2text)
 
+;; (setq mu4e-html2text-command "html2text -width 72 -nobs -utf8")
+;; (setq mu4e-html2text-command nil)
+
+(prog0
+ (custom-set-variables
+  '(mu4e-refile-folder "/Archive")
+  '(mu4e-sent-folder "/Archive")
+  '(mu4e-trash-folder "/Deleted Items")
+  '(mu4e-drafts-folder "/Drafts"))
+
+ (custom-set-variables
+  '(mu4e-refile-folder "/[Gmail].All Mail")
+  '(mu4e-sent-folder "/[Gmail].All Mail")
+  '(mu4e-trash-folder "/[Gmail].Trash")
+  '(mu4e-drafts-folder "/[Gmail].Drafts")))
+
 (custom-set-variables
  '(mu4e-attachment-dir "~/Downloads")
  '(mu4e-date-format-long "%Y-%m-%d")
- '(mu4e-drafts-folder "/Drafts")
- '(mu4e-get-mail-command "offlineimap")
  '(mu4e-headers-date-format "%y-%m-%d")
+ '(mu4e-get-mail-command "offlineimap")
  '(mu4e-headers-fields (quote ((:human-date . 12) (:flags . 6) (:from . 22) (:subject))))
  '(mu4e-headers-leave-behavior (quote apply))
  '(mu4e-view-show-addresses t)
- '(mu4e-html2text-command
-   ;; "html2text -width 72 -nobs -utf8"
-   nil)
  '(mu4e-bookmarks (quote (("flag:unread AND NOT flag:trashed" "Unread messages" 117) ("flag:flagged OR maildir:/INBOX" "Flagged" 105) ("date:today..now" "Today's messages" 116) ("date:7d..now" "Last 7 days" 119) ("mime:image/*" "Messages with images" 112))))
- '(mu4e-confirm-quit nil)
- '(mu4e-refile-folder "/Archive")
- '(mu4e-sent-folder "/Archive")
- '(mu4e-trash-folder "/Deleted Items"))
+ '(mu4e-confirm-quit nil))
+
+;; (custom-save-all)
 
 (provide 'rc-mu4e)
